1caedff958066d75fe283637acd7e94b9972642f
Muhammad Fuhad MK <muhammad.fuhad.dev@gmail.com>
2025-06-30 10:07:31 +0530

    frfrfrfrfrfrfrfr

--- user_app/schema.py
+++ user_app/schema.py
@@ -8,6 +8,7 @@ class Mutation(graphene.ObjectType):
     login_user = LoginUser.Field()
     comment_news = CommentNews.Field()
     like_news = LikeNews.Field()
+    update_user_profile = UpdateUserProfile.Field()
 
     verify_token = graphql_jwt.Verify.Field()
     refresh_token = graphql_jwt.Refresh.Field()
