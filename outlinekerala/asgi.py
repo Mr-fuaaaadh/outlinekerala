2760aa25720b4c33517b967201ceb9d5d6ae7d65
Muhammad Fuhad MK <muhammad.fuhad.dev@gmail.com>
2025-08-22 12:59:59 +0530

    FwefEWF

--- outlinekerala/asgi.py
+++ outlinekerala/asgi.py
@@ -1,16 +1,25 @@
 """
-ASGI config for outlinekerala project.
-
-It exposes the ASGI callable as a module-level variable named ``application``.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/5.1/howto/deployment/asgi/
+ASGI config for outlinekerala project with WebSocket support.
 """
 
 import os
-
 from django.core.asgi import get_asgi_application
+from channels.routing import ProtocolTypeRouter, URLRouter
+from channels.auth import AuthMiddlewareStack
+from django.urls import path
+from graphene_subscriptions.consumers import GraphqlSubscriptionConsumer
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'outlinekerala.settings')
 
-application = get_asgi_application()
+# HTTP -> normal Django
+django_asgi_app = get_asgi_application()
+
+# ASGI application with WebSockets enabled
+application = ProtocolTypeRouter({
+    "http": django_asgi_app,
+    "websocket": AuthMiddlewareStack(
+        URLRouter([
+            path("graphql/", GraphqlSubscriptionConsumer.as_asgi()),
+        ])
+    ),
+})
