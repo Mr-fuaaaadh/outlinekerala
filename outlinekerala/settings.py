8a3794fb69903774a85ca9ce3ae6e5a3753f2b08
Muhammad Fuhad MK <muhammad.fuhad.dev@gmail.com>
2025-09-19 16:53:28 +0530

    allowd host add iP

--- outlinekerala/settings.py
+++ outlinekerala/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = 'django-insecure-@5zhj15)!_uf&@m1%h3&fiz8z1(oiaefs_2z4vre5ex+tzze!4
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["outlinekerala.com", "www.outlinekerala.com", "backend.outlinekerala.com", "www.backend.outlinekerala.com"]
+ALLOWED_HOSTS = ["outlinekerala.com", "www.outlinekerala.com", "backend.outlinekerala.com", "www.backend.outlinekerala.com", "103.253.178.27"]
 
 
 # Application definition
